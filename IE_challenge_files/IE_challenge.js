dfe679c37a2bb878d30897fa79f3875dfbdce363
Anna Lisa Gentile <annalisa.gentile@ibm.com>
2019-01-23 21:52:32 -0800

    test

---
+++ IE_challenge_files/IE_challenge.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20151013
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('IE_challenge_files/IE_challengeMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ Overview_files/Overview.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20151013
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWStrokeParts([{rect:new IWRect(-2,2,4,245),url:'Overview_files/stroke.png'},{rect:new IWRect(-2,-2,4,4),url:'Overview_files/stroke_1.png'},{rect:new IWRect(2,-2,349,4),url:'Overview_files/stroke_2.png'},{rect:new IWRect(351,-2,4,4),url:'Overview_files/stroke_3.png'},{rect:new IWRect(351,2,4,245),url:'Overview_files/stroke_4.png'},{rect:new IWRect(351,247,4,4),url:'Overview_files/stroke_5.png'},{rect:new IWRect(2,247,349,4),url:'Overview_files/stroke_6.png'},{rect:new IWRect(-2,247,4,4),url:'Overview_files/stroke_7.png'}],new IWSize(353,249)),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Overview_files/OverviewMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
